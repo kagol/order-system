569d17f43004469608700441b0ed9d228c6bece3
Kagol <kagol@sina.com>
2025-03-16 18:46:04 +0800

    feat: add create order page

--- nestJs/src/menu/menu.service.ts
+++ nestJs/src/menu/menu.service.ts
@@ -134,6 +134,7 @@ export class MenuService {
       menu[29].parentId = menuId[25];
       menu[30].parentId = menuId[25];
       menu[33].parentId = menuId[32];
+      menu[34].parentId = menuId[32];
     }
     for (const item of menu) {
       await this.menu.update(item.id, { parentId: item.parentId });
--- web/src/api/order.ts
+++ web/src/api/order.ts
@@ -9,3 +9,8 @@ export function getOrderList(params) {
 export function deleteOrder(params) {
   return axios.delete(`/api/order`, { params })
 }
+
+// 创建订单
+export function createOrder(params) {
+  return axios.post(`/api/order`, params)
+}
